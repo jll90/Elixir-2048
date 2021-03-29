e7156d64a9656fb4f6bdfbea0159e148352367f4
Jesus Luongo Lizana <j2l@j2l-osx.local>
2021-03-29 17:31:53 -0300

    bumpv

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Engine2048.MixProject do
   def project do
     [
       app: :engine2048,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
